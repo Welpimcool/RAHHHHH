2c208fc0a94d4d8ca2e0848422dcb6412a749dea
Welpimcool <bo1bo2bo3bo1bo1bo1@gmail.com>
2024-10-15 14:47:10 -0700

    Sword attack

--- Chance class game jam/Assets/Scripts/attack.cs
+++ Chance class game jam/Assets/Scripts/attack.cs
@@ -6,19 +6,33 @@ using UnityEngine.UIElements;
 public class attack : MonoBehaviour
 {
     private BoxCollider hitbox;
+    private MeshRenderer renderer;
 
     // Start is called before the first frame update
     void Start()
     {
         hitbox = GetComponent<BoxCollider>();
+        renderer = GetComponentInChildren<MeshRenderer>();
+        transform.Rotate(0, -55.0f, 0, Space.Self);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("Space"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            StartCoroutine(Rotate());
         }
+    }
+    IEnumerator Rotate()
+    {
+        renderer.enabled = true;
+        for (int i = 0; i <22; i++)
+        {
+            transform.Rotate(0, 5.0f, 0, Space.Self);
+            yield return new WaitForSeconds(.01f);
+        }
+        renderer.enabled = false;
+        transform.Rotate(0, -110.0f, 0, Space.Self);
     }
 }
